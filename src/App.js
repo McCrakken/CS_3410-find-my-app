5859133ed64f892703c9e2a318eb80997f5df9ab
McKay Larsen <mckay.larsen@microfocus.com>
2019-11-26 11:26:45 -0700

    Add example component <Card/> with styling and import

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Import your component into the page you intend to use it, convention looks like this
+import Card from './components/Card/Card';
+
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +21,22 @@ function App() {
         >
           Learn React
         </a>
+        {/*Add your component and pass appropriate props, this is missing.text here because I wanted the <code/> style*/}
+        <Card image={logo}
+              title={'React as Card'}
+        >
+          {/*You can pass other HTML looking elements (JSX) into your component and generate them as props.children*/}
+          <p className='card-text'>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a className="btn btn-outline-success"
+             href="https://reactjs.org"
+             target="_blank"
+             rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </Card>
       </header>
     </div>
   );
