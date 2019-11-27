76aa048caee7c4de22ebdb3f31446022f5169056
McKay Larsen <mckay.larsen@microfocus.com>
2019-11-27 13:43:01 -0700

    Add initial hoc for page-content layout, remove App.css

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-//Import your component into the page you intend to use it, convention looks like this
 import Card from './components/Card/Card';
+import Nav from './components/Navbar/Navbar'
+import Content from './hoc/PageContent'
 
 function App() {
   return (
     <div className="">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/*Add your component and pass appropriate props, this is missing.text here because I wanted the <code/> style*/}
-        <Card image={logo}
-              title={'React as Card'}
-        >
-          {/*You can pass other HTML looking elements (JSX) into your component and generate them as props.children*/}
-          <p className='card-text'>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="btn btn-outline-success"
-             href="https://reactjs.org"
-             target="_blank"
-             rel="noopener noreferrer"
+      <Nav/>
+      <Content>
+        <header className="App-header">
+          {/*Add your component and pass appropriate props, this is missing.text here because I wanted the <code/> style*/}
+          <Card image={logo}
+                title={'React as Card'}
           >
-            Learn React
-          </a>
-        </Card>
-      </header>
+            {/*You can pass other HTML looking elements (JSX) into your component and generate them as props.children*/}
+            <p className='card-text'>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a className="btn btn-outline-success"
+               href="https://reactjs.org"
+               target="_blank"
+               rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </Card>
+        </header>
+      </Content>
     </div>
   );
 }
---
+++ src/hoc/Layout/Side-Panel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const sidepanel = (props) => {
+  return (
+    <div className={'col-sm-4 col-md-3'}>
+      Side-Panel Content
+    </div>
+  )
+};
+
+export default sidepanel;
