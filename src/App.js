01ae90541913b84bf0d6aed4858a57dfffe2c7eb
McKay <10561310@my.uvu.edu>
2019-12-02 22:03:33 -0700

    Remove React boiler-plate from App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 
+import EventCardContainer from "./containers/EventCard/EventCardContainer";
 import Card from './components/Card/Card';
 import EventCard from "./components/Card/EventCard/EventCard";
 import Nav from './components/Navbar/Navbar'
@@ -11,38 +12,7 @@ function App() {
     <div className="">
       <Nav/>
       <Content>
-        <header className="App-header">
-          {/*Add your component and pass appropriate props, this is missing.text here because I wanted the <code/> style*/}
-          <Card image={logo}
-                title={'React as Card'}
-                classes={'card card-panel'}
-          >
-            {/*You can pass other HTML looking elements (JSX) into your component and generate them as props.children*/}
-            <p className='card-text'>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a className="btn btn-outline-success"
-               href="https://reactjs.org"
-               target="_blank"
-               rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </Card>
-          <Card
-            classes={'card'}
-            title={'Not Big'}
-                text={'This is a small card'}>
-            <p>This is just some content</p>
-            <button className={'btn btn-danger'}>Button</button>
-          </Card>
-          <EventCard
-            image={logo}
-            title={'EventCard title'}
-            text={'This is a simple event card example'}
-          />
-
-        </header>
+          <EventCardContainer/>
       </Content>
     </div>
   );
