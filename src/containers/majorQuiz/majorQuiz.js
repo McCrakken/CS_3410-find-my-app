99c4efb5d63d2d0411d8b311b5de6d6fd3b5eefd
McKay Larsen <mckay.larsen@microfocus.com>
2019-12-05 13:17:21 -0700

    Forgot to add MajorLeftPanel for some reason

--- src/containers/majorQuiz/majorQuiz.js
+++ src/containers/majorQuiz/majorQuiz.js
@@ -1,18 +1,24 @@
-import React,{Component} from 'react';
-import Content from '../../hoc/PageContent';
-import SidePanel from "../../hoc/Layout/Side-Panel";
-import Main from "../../hoc/Layout/Main";
-import QuizContent from "./quizContent";
+import React, { Component } from 'react';
 
-class MajorQuiz extends Component{
-    render(){
-        return(
-            <Content>
-                <SidePanel/>
-                <Main>
-                    <QuizContent/>
-                </Main>
-            </Content>
-        )
-    }
+import Content from '../../hoc/PageContent';
+import SidePanel from '../../hoc/Layout/Side-Panel';
+import Main from '../../hoc/Layout/Main';
+import QuizContent from "./quizContent";
+import MajorLeftPanel from "./MajorLeftPanel";
+
+class MajorQuiz extends Component {
+  render() {
+    return(
+      <Content>
+        <MajorLeftPanel />
+          <Main heading={'In-Depth Assessment'}>
+            <QuizContent/>
+        </Main>
+      </Content>
+        
+    )
+
+  }
 }
+
+export default MajorQuiz;
