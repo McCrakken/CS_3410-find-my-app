2f92df1c1c7820735bad13997c1e133b3d22e209
kellieburch <kellieb94@hotmail.com>
2019-12-05 03:13:58 -0500

    DegreeChangesPage

--- src/DegreeChanges.js
+++ src/DegreeChanges.js
@@ -1,4 +1,10 @@
 import React, { Component } from "react";
+import './App.css';
+//import {Chart} from 'react-charts'
+import {Helmet} from "react-helmet";
+import Logo from './Logo.gif';
+import Icon from './changeIcon.png';
+
 
 import EventCardContainer from "./containers/EventCard/EventCardContainer";
 import Card from './components/Card/Card';
@@ -7,31 +13,88 @@ import Nav from './components/Navbar/Navbar'
 import Content from './hoc/PageContent'
 import SidePanel from './hoc/Layout/Side-Panel';
 
- 
+import MyBarChart from './MyBarChart';
+import data from './data.json';
+
+
 class DegreeChanges extends Component {
   render() {
     return (
       <div>
+        <div class="flex-container">
+          <div class="size1">
+            <div>
+              <button class="btnSize">Change Degree</button>
+            </div>
+            <div class="divider"></div>
+            <div>
+            <button class="btnSize">Set Up Meeting With Councilor</button>
+            </div>
+            <div class="divider"></div>
+            <div class="size1">
+              Potential Degrees
+              <div class="degreeName">
+              <div class="flip-card">
+            <div class="flip-card-inner">
+              <div class="flip-card-front">
+              <MyBarChart data={data} />
+  
+              </div>
+              <div class="flip-card-back">
+                <h1>Degree Name</h1> 
+                <p>Architect  Engineer</p> 
+                <p>Degree Details</p>
+              </div>
+            </div>
+          </div>
+              </div>
+            </div>
+          </div>
+          <div class="size2">
+          <div id="myDIV" class="header">
+            <h2 >Degree Updates</h2>
+          </div>
+
+          <ul id="myUL">
+            <li>
+            <img src={Icon} style={{width:70, height: 70, marginTop: 10, marginRight: 10, padding: 3 }}></img>
+              <h3>Class Name</h3>
+              <p> Hit the gym</p>
+             </li>
+            <li class="checked">
+            <img src={Icon} style={{width:70, height: 70, marginTop: 10, marginRight: 10, padding: 3 }}></img>
+              <h3>Class Name</h3>
+              <p> Pay bills</p>
+              </li>
+            <li>
+            <img src={Icon} style={{width:70, height: 70, marginTop: 10, marginRight: 10, padding: 3 }}></img>
+              <h3>Class Name</h3>
+              <p> Meet George</p>
+            </li>
+            <li>
+            <img src={Icon} style={{width:70, height: 70, marginTop: 10, marginRight: 10, padding: 3 }}></img>
+              <h3>Class Name</h3>
+              <p> Read a book</p>
+            </li>
+            <li>
+            <img src={Icon} style={{width:70, height: 70, marginTop: 10, marginRight: 10, padding: 3 }}></img>
+              <h3>Class Name</h3>
+              <p>  Organize office</p>
+            </li>
+          </ul>
+          </div>  
+        </div>
         <div className='main-content col-sm-8 col-md-9'>
-        <h2>Degree Updates</h2>
-    <div className={'side-panel col-sm-4 col-md-3'}>
-       <button>Change Deree</button>
-        <button>Set Up Meeting With Councilor</button>
-    </div>
 
      <div className="PageContent">
     </div>
-        <div>
-        <div>
-        <Card title="Required Classes">
-            <EventCardContainer title="Class Name"/>
-        </Card>
-        </div>
-        </div>
       </div>
       </div>
+     
+      
     );
   }
 }
+
  
 export default DegreeChanges;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -19,14 +19,14 @@ const nav = (props) => {
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+        <li className="nav-item ">
+        <a className="nav-link" href="/">R <span className="sr-only">(current)</span></a>
           </li>
-          <li className="nav-item ">
+          <li class="link" className="nav-item ">
             <a className="nav-link" href="/Reminders"><NavLink exact to="/reminders">Reminders</NavLink></a>
           </li>
           <li className="nav-item ">
-            <a className="nav-link" href="/DegreeChanges"><NavLink exact to="/degreeChanges">Degree Changes</NavLink></a>
+            <a class="link" className="nav-link" href="/DegreeChanges"><NavLink exact to="/degreeChanges">Degree Changes</NavLink></a>
           </li>
           <li className="nav-item ">
             <a className="nav-link" href="/DegreePath"><NavLink exact to="/degreePath">Degree Path/Plan</NavLink></a>
