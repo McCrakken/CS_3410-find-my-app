7466086a3b601dc6d20474da27335aeed5d6fa0c
McKay Larsen <mckay.larsen@microfocus.com>
2019-12-05 12:47:18 -0700

    Containerize Kellie's DegreeChanges page add styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './styles/custom.scss';
-import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
