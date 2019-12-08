6eae004a75c8d78e6e40ad53368eef921dfc12c0
McKay Larsen <mckay.larsen@microfocus.com>
2019-12-07 22:03:52 -0700

    Remove unused stuff

--- src/containers/SignUp/SignUp.js
+++ src/containers/SignUp/SignUp.js
@@ -4,43 +4,9 @@ import Content from '../../hoc/PageContent';
 import SidePanel from '../../hoc/Layout/Side-Panel';
 import Main from '../../hoc/Layout/Main';
 import SignUpContainer from './SignUpCard/SignUpContainer';
-import Modal from "../../components/Modal/Modal";
 import ClassesContainer from "./ClassesCard/ClassesContainer";
 
 class SignUp extends Component {
-  state = {
-    Heading: 'Reminders',
-    HeadingBtnText: 'Add Event',
-    AddBtnText: 'Add Event',
-    AddModalText: 'Event',
-    AddBtnDisplay: true,
-    hasModal: true,
-    SubSection: [
-      { heading: 'Dec 9, 2019 - Monday',
-        Notes: [
-          { title: 'Final Presentation',
-            text: 'Groups will present their High-Def prototypes and explain their development process.',
-            borderColor: 'green-1'
-          },
-          { title: 'High-def Prototype',
-            text: 'Submit source code and link to a deployed server for group projects.',
-            borderColor: 'green-1'
-          },
-          { title: 'Final Exam',
-            text: 'Final cumulative exam, 3:00pm - 4:50',
-            borderColor: 'yellow-1'
-          },
-        ]},
-      { heading: 'Dec 11, 2019 - Wednesday',
-        Notes: [
-          { title: 'Final Exam',
-            text: 'Final exam requires students to use Java to implement different build patterns',
-            borderColor: 'blue-1'
-          },
-        ]
-      }],
-  };
-
   render() {
     return(
         <Content>
--- src/containers/SignUp/SignUpCard/SignUpContainer.js
+++ src/containers/SignUp/SignUpCard/SignUpContainer.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component } from 'react';
 
 import NotesContainer from "../../Events/Notes/NotesContainer";
 
